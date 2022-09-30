fbeac21ff6d9d6813b687255904d96da57d34313
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-09-30 21:52:23 +0530

    #include<stdio.h> int main() {     int t;     scanf("%d",&t);     for(int i=0;i<t;i++)     {         int a,b,res;         scanf("%d%d",&a,&b);         res=a+b;         printf("%d",res);         printf(" ");     }     return 0; }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    for(int i=0;i<t;i++)
+    {
+        int a,b,res;
+        scanf("%d%d",&a,&b);
+        res=a+b;
+        printf("%d",res);
+        printf("
+");
+    }
+    return 0;
+}
